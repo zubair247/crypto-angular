03ad9e95646f765706ed64de9ef236c94affbeb2
Zubair Moosa <mzmoosa01@gmail.com>
2020-07-15 19:23:15 +0200

    Created nx workspace

---
+++ crypto-angular/apps/crypto-frontend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'crypto-frontend',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/crypto-frontend',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
